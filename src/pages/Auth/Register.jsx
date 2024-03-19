6315d07cff7cf48bc95fca3c5465ff8590e088dc
Igor Martins <igor.martins@smartnx.com>
2024-03-19 17:51:55 -0300

    Criando tela de registro

--- src/pages/Auth/Register.jsx
+++ src/pages/Auth/Register.jsx
@@ -1,8 +1,30 @@
 import "./Auth.css"
 
+//components
+import { Link } from "react-router-dom"
+
+//hooks
+import { useState, useEffect } from "react"
+
+const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log('clicou');
+}
+
 const Register = () => {
     return (
-        <div>Register</div>
+        <div id="register">
+            <h2>ReactGram</h2>
+            <p className="subtitle">Cadastres-se para ver as fotos dos seus amigos</p>
+            <form onSubmit={handleSubmit}>
+                <input type="text" placeholder="Nome"/>
+                <input type="email" placeholder="E-mail"/>
+                <input type="password" placeholder="Senha"/>
+                <input type="password" placeholder="Confirme a senha"/>
+                <input type="submit" value='Cadastrar'/>
+            </form>
+            <p>Já tem conta? <Link to='/login'>Clique aqui</Link></p>
+        </div>
     )
 }
 
